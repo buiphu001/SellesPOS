58e102fc63a1d263f2961c3ef29775a7723a5059
BuiPhu <buiphu001@gmail.com>
2020-04-23 15:59:33 +0700

    Initial commit

---
+++ app/src/main/java/com/buiphu/sellespos/ui/trogiup.java
@@ -0,0 +1,29 @@
+package com.buiphu.sellespos.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.buiphu.sellespos.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class trogiup extends Fragment {
+
+    public trogiup() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trogiup, container, false);
+    }
+}
